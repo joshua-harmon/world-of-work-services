024a9cb8d4c49fce93a193eac7e68290ba6e55ed
joshua-harmon <joshua.harmon1960@gmail.com>
2020-10-23 11:31:05 -0700

    edits

--- src/components/Helloworld.vue
+++ src/components/Helloworld.vue
@@ -10,7 +10,26 @@
     <h2>professional positions based upon experience and education</h2>
     <h2>self-employment can immediately become part of the coop and can make money</h2>
     <h2>employment-work options</h2>
-
+<form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
   </div>
 </template>
 
